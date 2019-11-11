7c5e61dd87cd3a9d26b9c612ba0ccf102bfbd6f5
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-11 12:12:08 +0100

    Refactored camera service.

--- Source/Scanner3DClient/Services/Camera/CameraService.cpp
+++ Source/Scanner3DClient/Services/Camera/CameraService.cpp
@@ -1,29 +1,7 @@
 #include "pch.h"
 #include "CameraService.h"
 
-#include <iostream>
-
 using namespace Scanner3DClient::Services;
-
-bool CameraService::SendGetConfigRequest(GetConfigResponseCallback&& callback)
-{
-    return SendRequest(Request{ 'g', {} }, [this, callback = std::move(callback)](auto&& response)
-    {
-        OnConfigResponse(callback, std::move(response));
-    });
-}
-
-bool CameraService::SendApplyConfigRequest(const CameraConfig& config, GetConfigResponseCallback&&callback)
-{
-    const auto beginIterator = reinterpret_cast<const RemoteServices::byte*>(&config);
-    const auto endIterator = beginIterator + sizeof(CameraConfig);
-    auto payload = RemoteServices::ServicePayload{ beginIterator, endIterator };
-
-    return SendRequest(Request{ 'a', std::move(payload) }, [this, callback = std::move(callback)](auto&& response)
-    {
-        OnConfigResponse(callback, std::move(response));
-    });
-}
 
 bool CameraService::SendCaptureImageRequest(CaptureImageResponseCallback&& callback)
 {
@@ -37,21 +15,4 @@ bool CameraService::SendCaptureImageRequest(CaptureImageResponseCallback&& callb
         else
             callback(std::move(response.Payload));
     });
-}
-
-void CameraService::OnConfigResponse(const GetConfigResponseCallback& callback, Response&& response)
-{
-    if (!callback)
-        return;
-
-    if (response.Type != Response::ResponseType::Ok)
-        return callback(std::nullopt);
-
-    auto& payload = response.Payload;
-    CLIENT_ASSERT(payload.size() == sizeof(CameraConfig));
-    if (payload.size() != sizeof(CameraConfig))
-        callback(std::nullopt);
-
-    const auto cameraConfig = reinterpret_cast<CameraConfig*>(payload.data());
-    callback(*cameraConfig);
 }
--- Source/Scanner3DClient/Services/Camera/CameraService.h
+++ Source/Scanner3DClient/Services/Camera/CameraService.h
@@ -7,29 +7,12 @@ namespace Scanner3DClient::Services
     class CameraService final : public RemoteServices::SingleConnectionRequestResponseServiceBase
     {
     public:
-        struct CameraConfig
-        {
-            unsigned short Width;           //multiple of 320
-            unsigned short Height;          //multiple of 240
-            byte Brightness;                //0 to 100
-            byte Sharpness;                 //0 to 100
-            byte Contrast;                  //0 to 100
-            unsigned short ISO;             //100 to 800
-            byte Saturation;                //0 to 100
-        };
-
-        using GetConfigResponseCallback = std::function<void(std::optional<CameraConfig>)>;
         using CaptureImageResponseCallback = std::function<void(std::vector<byte>&&)>;
 
     public:
         CameraService() = default;
         virtual ~CameraService() override final = default;
 
-        bool SendGetConfigRequest(GetConfigResponseCallback&& callback);
-        bool SendApplyConfigRequest(const CameraConfig& config, GetConfigResponseCallback&& callback);
         bool SendCaptureImageRequest(CaptureImageResponseCallback&& callback);
-
-    private:
-        static void OnConfigResponse(const GetConfigResponseCallback& callback, Response&& response);
     };
 }
